a75fd3a5475d856e14ddc41b4574e5bb569f857e
Taluama Velludo <taluama@gmail.com>
2019-07-14 08:35:26 -0400

    mid-process

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,48 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  console.log('IronGenerator JS imported successfully!');
+  function printTheShifts(){
+    let list = document.getElementById('shifts-list');
+    axios.get('http://localhost:3000/shift/api')
+    .then((response)=>{
+      let arrayOfShifts = response.data.reverse();
+      list.innerHTML = "";
+      arrayOfShifts.forEach((eachShift)=>{
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute("class", "each-shift");
+        newDiv.innerHTML = `
+        <h4>${eachShift.assigned}: ${eachShift.start} - ${eachShift.end}</h4>
+        `
+        list.appendChild(newDiv);
+      })
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
+  printTheShifts();
+
+  let addButton = document.getElementById('add-btn');
+  addButton.onclick = ()=>{
+    let assigned = document.getElementById('assigned');
+    let start = document.getElementById('start');
+    let end = document.getElementById('end');
+
+    axios.post('http://localhost:3000/shifts/api', {
+      assigned: assigned.value,
+      start: start.value,
+      end: end.value
+    })
+    .then((res)=>{
+        printTheShifts();
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+
+    assigned.value = "";
+    start.value = "";
+    end.value = "";
+  }
 
 }, false);
--- routes/shift-routes.js
+++ routes/shift-routes.js
@@ -1,9 +1,70 @@
 const express = require('express');
 const router = express.Router();
+const Shifts = require('../models/Shift');
 
 /* GET shifts page*/
 router.get('/', (req, res, next) => {
   res.render('shift-views/index');
 });
 
+
+router.get('/api', (req, res, next)=>{
+
+  Shifts.find()
+  .then((listOfShifts)=>{
+    res.json(listOfShifts);
+  })
+  .catch((err)=>{
+    next(err);
+  })
+})
+
+router.post('/api', (req, res, next)=>{
+  let assigned = req.body.assigned;
+  let start = req.body.start;
+  let end = req.body.end;
+
+  Shifts.create({
+    assigned: assigned,
+    start: start,
+    end: end
+  })
+  .then((response)=>{
+    // res.json(); // in case message doesn't work
+    res.json({message: 'Successfully creted a shift!'});
+  })
+  .catch((err)=>{
+    res.json(err);
+  })
+})
+
+
+//?????????????????
+// router.post('/', (req, res, next)=>{
+//   const newShift = new Shifts(req.body);
+//   newShift.save()
+//   .then(()=>{
+//     req.flash('error', "✓");
+//     res.redirect('/shifts');
+//   })
+//   .catch((err)=>{
+//     res.redirect('/celebrities/new');
+//     next(err);
+//   })
+// })
+
+
+
+
+
+
+
+
+
+
+
 module.exports = router;
+
+
+
+
