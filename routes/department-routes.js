137dec2e798d154fe2360861f7a29c0cf69c31a0
Michael Roth <michael.franklin.roth@gmail.com>
2019-07-15 09:03:49 -0400

    routes for department are finsihed

--- routes/department-routes.js
+++ routes/department-routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const Department = require('../models/Department')
 
 router.get('/', (req, res, render) => {
   res.render('department-views/all-departments')
@@ -8,6 +9,29 @@ router.get('/', (req, res, render) => {
 
 router.get('/sampleDepartment', (req, res, render) => {
   res.render('department-views/one-department')
+})
+
+router.get('/create', (req, res, render) => {
+  res.render('department-views/create-department')
+})
+
+router.post('/create', (req, res, render) => {
+  req.flash('success', 'create button was called.  Good job!')
+  res.redirect('/departments')
+})
+
+router.get('/sampleDepartment/edit', (req, res, render) => {
+  res.render('department-views/edit-one-department')
+})
+
+router.post('/sampleDepartment/update', (req, res, render) => {
+  req.flash('success', 'update button was called.  Good job!')
+  res.redirect('/departments')
+})
+
+router.post('/sampleDepartment/delete', (req, res, render) => {
+  req.flash('success', 'delete button was called.  Good job!')
+  res.redirect('/departments')
 })
 
 router.get('/sampleDepartment/thisEmployee', (req, res, render) => {
