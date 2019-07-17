f9003ffad62872cdc54b5c6613d1c90c49592d55
Taluama Velludo <taluama@gmail.com>
2019-07-17 12:31:17 -0400

    shifts grid created

--- public/javascripts/schedule.js
+++ public/javascripts/schedule.js
@@ -1,10 +1,17 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-  // $('#schedule').append($('div')
-  // <div>
-  //   <input type="checkbox" id="mo2/etc" name="code" value="mo2/etc">
-  //   <label for="coding">Coding</label>
-  // </div>
-
-
+  let arrayOfDaysOfTheWeek = ['su', 'mo', 'tu', 'we', 'th', 'fr', 'sa'];
+  let table = document.getElementById('schedule');
+    table.innerHTML = "";
+    for (j = 1; j <= 3; j++){
+      for (i = 0; i <= (arrayOfDaysOfTheWeek.length-1); i++){
+        let newDiv = document.createElement('div');
+        newDiv.setAttribute("class", "each-slot");
+        newDiv.innerHTML = `
+          <input type="checkbox" id="${arrayOfDaysOfTheWeek[i]}${j}" name="code" value="${arrayOfDaysOfTheWeek[i]}${j}">
+          <p class="slot-label">${arrayOfDaysOfTheWeek[i]}${j}</p>
+        `
+        table.appendChild(newDiv);
+      }
+    }
 });
