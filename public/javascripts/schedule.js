09c64275f7362e073846a014826f02a2f759fc62
Taluama Velludo <taluama@gmail.com>
2019-07-17 11:09:08 -0400

    started

--- public/javascripts/schedule.js
+++ public/javascripts/schedule.js
@@ -2,12 +2,8 @@ $(document).ready(function () {
 
   // $('#schedule').append($('div')
   // <div>
-  //   <input type="checkbox" id="coding" name="interest" value="coding">
+  //   <input type="checkbox" id="mo2/etc" name="code" value="mo2/etc">
   //   <label for="coding">Coding</label>
-  // </div>
-  // <div>
-  //   <input type="checkbox" id="music" name="interest" value="music">
-  //   <label for="music">Music</label>
   // </div>
 
 
