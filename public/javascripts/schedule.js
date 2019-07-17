f35dbf2b1f72e13978a0485c6f05bc854a880ffd
Michael Roth <michael.franklin.roth@gmail.com>
2019-07-17 15:37:28 -0400

    updated dummy data for Talu

--- public/javascripts/schedule.js
+++ public/javascripts/schedule.js
@@ -1,17 +1,17 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   let arrayOfDaysOfTheWeek = ['su', 'mo', 'tu', 'we', 'th', 'fr', 'sa'];
   let table = document.getElementById('schedule');
-    table.innerHTML = "";
-    for (j = 1; j <= 3; j++){
-      for (i = 0; i <= (arrayOfDaysOfTheWeek.length-1); i++){
-        let newDiv = document.createElement('div');
-        newDiv.setAttribute("class", "each-slot");
-        newDiv.innerHTML = `
-          <input type="checkbox" id="${arrayOfDaysOfTheWeek[i]}${j}" name="code" value="${arrayOfDaysOfTheWeek[i]}${j}">
+  table.innerHTML = "";
+  for (j = 1; j <= 3; j++) {
+    for (i = 0; i <= (arrayOfDaysOfTheWeek.length - 1); i++) {
+      let newDiv = document.createElement('div');
+      newDiv.setAttribute("class", "each-slot");
+      newDiv.innerHTML = `
+          <input type="checkbox" id="${arrayOfDaysOfTheWeek[i]}${j}" name="codes" value="${arrayOfDaysOfTheWeek[i]}${j}">
           <p class="slot-label">${arrayOfDaysOfTheWeek[i]}${j}</p>
         `
-        table.appendChild(newDiv);
-      }
+      table.appendChild(newDiv);
     }
+  }
 });
