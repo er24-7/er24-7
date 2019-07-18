3e6362507d3ac671d3ce409ad677d9d03e2334a6
Michael Roth <michael.franklin.roth@gmail.com>
2019-07-18 17:42:55 -0400

    finally got something

--- models/Shift.js
+++ models/Shift.js
@@ -4,18 +4,18 @@ const Schema = mongoose.Schema;
 const shiftSchema = new Schema({
 
   assigned: { type: Schema.Types.ObjectId, ref: "User" },
-  // codes: [{
-  //   type: String, enum: [
-  //     "su1", "su2", "su3",
-  //     "mo1", "mo2", "mo3",
-  //     "tu1", "tu2", "tu3",
-  //     "we1", "we2", "we3",
-  //     "th1", "th2", "th3",
-  //     "fr1", "fr2", "fr3",
-  //     "sa1", "sa2", "sa3",
-  //   ]
-  // }]
-  codes: [{ type: String }]
+  codes: [{
+    type: String, enum: [
+      "su1", "su2", "su3",
+      "mo1", "mo2", "mo3",
+      "tu1", "tu2", "tu3",
+      "we1", "we2", "we3",
+      "th1", "th2", "th3",
+      "fr1", "fr2", "fr3",
+      "sa1", "sa2", "sa3",
+    ]
+  }]
+  // codes: [{ type: String }]
 
 })
 
