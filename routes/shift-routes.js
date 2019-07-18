4412fcd6a171e4554f0cc161826e692a8cdd02c0
Michael Roth <michael.franklin.roth@gmail.com>
2019-07-18 17:11:41 -0400

    some changes

--- routes/shift-routes.js
+++ routes/shift-routes.js
@@ -17,13 +17,13 @@ router.get('/:deptName', (req, res, next) => {
             .then((allShiftsWithinDepartment) => {
 
               allShiftsWithinDepartment.forEach((eachShift) => {
-                eachShift.codes.forEach(eachCode => {
-                  eachCode = "something";
-                  eachCode.blah = "blah"
+                eachShift.codes.forEach((eachCode) => {
+                  eachCode = "blah"
                   console.log(eachCode)
-                  console.log(eachCode.blah)
                 })
+                console.log(eachShift)
               })
+              console.log(allShiftsWithinDepartment)
               res.render('shift-views/create-shift', { users: allUsers, department: theDepartment, shifts: allShiftsWithinDepartment });
             })
             .catch((err) => {
