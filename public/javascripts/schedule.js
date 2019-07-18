41a5aa1ea07f729fcdc393c16cc9acdb904383a4
Taluama Velludo <taluama@gmail.com>
2019-07-17 20:19:11 -0400

    working on tiles

--- public/javascripts/schedule.js
+++ public/javascripts/schedule.js
@@ -5,12 +5,15 @@ $(document).ready(function () {
   table.innerHTML = "";
   for (j = 1; j <= 3; j++) {
     for (i = 0; i <= (arrayOfDaysOfTheWeek.length - 1); i++) {
-      let newDiv = document.createElement('div');
-      newDiv.setAttribute("class", "each-slot");
-      newDiv.innerHTML = `
+      // let newAnch = document.createElement('a');
+      let newAnch = document.createElement('div');
+      newAnch.setAttribute("tabindex", "1");
+      newAnch.setAttribute("class", "each-slot");
+      // newAnch.setAttribute("href", "#");
+      newAnch.innerHTML = `
           <input type="checkbox" id="${arrayOfDaysOfTheWeek[i]}${j}" name="codes" value="${arrayOfDaysOfTheWeek[i]}${j}">
         `
-      table.appendChild(newDiv);
+      table.appendChild(newAnch);
     }
   }
 });
