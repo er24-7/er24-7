ab2fdcc489985ed84ae39da6029590eed81bf3ef
Michael Roth <michael.franklin.roth@gmail.com>
2019-07-18 22:18:19 -0400

    finish delete 1 shift btn functionality

--- routes/shift-routes.js
+++ routes/shift-routes.js
@@ -132,59 +132,23 @@ router.post('/:deptName/delete/all', (req, res, next) => {
 
 })
 
-// router.get('/api', (req, res, next) => {
 
-//   Shifts.find()
-//     .then((listOfShifts) => {
-//       res.json(listOfShifts);
-//     })
-//     .catch((err) => {
-//       next(err);
-//     })
-// })
+router.post('/delete/:id', (req, res, next) => {
+  Shifts.findByIdAndDelete(req.params.id)
+    .then((theShift) => {
+      User.findById(theShift.assigned).populate('department')
+        .then((theUser) => {
+          res.redirect('/shifts/' + theUser.department.name)
 
-// router.post('/api', (req, res, next) => {
-//   let assigned = req.body.assigned;
-//   let start = req.body.start;
-//   let end = req.body.end;
-
-//   Shifts.create({
-//     assigned: assigned,
-//     start: start,
-//     end: end
-//   })
-//     .then((response) => {
-//       // res.json(); // in case message doesn't work
-//       res.json({ message: 'Successfully creted a shift!' });
-//     })
-//     .catch((err) => {
-//       res.json(err);
-//     })
-// })
-
-
-//?????????????????
-// router.post('/', (req, res, next)=>{
-//   const newShift = new Shifts(req.body);
-//   newShift.save()
-//   .then(()=>{
-//     req.flash('error', "✓");
-//     res.redirect('/shifts');
-//   })
-//   .catch((err)=>{
-//     res.redirect('/celebrities/new');
-//     next(err);
-//   })
-// })
-
-
-
-
-
-
-
-
-
+        })
+        .catch((err) => {
+          next(err)
+        })
+    })
+    .catch((err) => {
+      next(err)
+    })
+})
 
 
 module.exports = router;
